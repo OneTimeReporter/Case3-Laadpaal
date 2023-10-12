6f4debc1a9da0b03b36427174cd7e5ff0dfbe515
OneTimeReporter <tarik_can@live.nl>
2023-10-12 14:29:48 +0200

    Update Laadpaaldataplots.py

--- pages/Laadpaaldataplots.py
+++ pages/Laadpaaldataplots.py
@@ -17,11 +17,11 @@ laadpaalDf['Ended'] = pd.to_datetime(laadpaalDf['Ended'], errors='coerce', forma
 st.write("We beginnen met het inladen van de dataset door het aan te roepen van 'laadpaaldata.csv'. ")
 st.write("De kolom 'Efficiency' wordt aangemaakt door 'Chargetime' te delen met 'ConnectedTime'. ")
 
-st.header("dtypes")
+st.header("Dataset Dtypes")
 st.write("De datatypes en algemene informatie van de dataset.")
 st.write(laadpaalDf.dtypes)
 st.divider()
-st.header("describe")
+st.header("Dataset Describe")
 st.write("De tijdskolommen zijn in uren, en de eenheid van energie gebruikt is kWh.")
 st.write(laadpaalDf.describe())
 st.divider() 
@@ -33,21 +33,20 @@ plt.hist(laadpaalDf['ChargeTime'], bins=10, range=(0, 10), color='skyblue', edge
 plt.xlabel('Laadtijd (Uren)')
 plt.ylabel('Waarnemingen')
 plt.title('Verdeling van Laadtijd')
-
-plt.show()
+st.pyplot()
 
 # Calculate the z-score for the ConnectedTime column
 z_scores = (laadpaalDf['ConnectedTime'] - laadpaalDf['ConnectedTime'].mean()) / laadpaalDf['ConnectedTime'].std()
-
 # Define a threshold for outliers (e.g., z-score > 3)
 outlier_threshold = 3
-
 # Drop rows where the z-score exceeds the threshold
 laadpaalDf = laadpaalDf[z_scores <= outlier_threshold]
 
 # Drop rows where the Efficiency column is equal to 0
 laadpaalDf = laadpaalDf[laadpaalDf['Efficiency'] != 0]
 
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Sort the data by ConnectedTime
 sorted_data = laadpaalDf.sort_values('ConnectedTime')
@@ -67,27 +66,4 @@ plt.colorbar(label='ConnectedTime')
 plt.plot(sorted_data['ConnectedTime'], trend_line(sorted_data['ConnectedTime']), color='red')
 
 plt.ylim(0)  # Set the Y axis lower limit to 0
-
-plt.show()
-
-# Sort the data by ConnectedTime
-sorted_data = laadpaalDf.sort_values('ConnectedTime')
-
-# Calculate the trend line using linear regression
-coefficients = np.polyfit(sorted_data['ConnectedTime'], sorted_data['Efficiency'], 1)
-trend_line = np.poly1d(coefficients)
-
-# Plot the scatter plot with smaller datapoints
-plt.scatter(laadpaalDf['MaxPower'], laadpaalDf['TotalEnergy'], c=laadpaalDf['ConnectedTime'], cmap='cool', alpha=0.5, s=10)
-plt.xlabel('ConnectedTime')
-plt.ylabel('Efficiency')
-plt.title('Scatter Plot of MaxPower vs Efficiency')
-plt.colorbar(label='ConnectedTime')
-
-# Plot the trend line
-plt.plot(sorted_data['ConnectedTime'], trend_line(sorted_data['TotalEnergy']), color='red')
-
-plt.ylim(0)  # Set the Y axis lower limit to 0
-
-plt.show()
-
+st.pyplot()
